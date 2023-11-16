920e93c405f6e4f55a2652279efa87a279fdc601
Ry Christofer Pahente <roxy0x7@proton.me>
2023-11-16 09:03:16 +0000

    added Web react

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const generatePassword = (length) => {
+    const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+    let password = '';
+    for (let i = 0; i < length; i++) {
+      password += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+    return password;
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Click the button to generate a password:</Text>
+      <Button title="Generate Password" onPress={() => alert(generatePassword(12))} />
       <StatusBar style="auto" />
     </View>
   );
