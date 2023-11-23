06bf32388a5cae8ddf6dd77f3d86815bb2bd19cb
mariikyo <20210022821@my.xu.edu.ph>
2023-11-23 17:13:46 +0800

    Updated password length and added character selection feature

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput, Picker } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 import React, { useState } from 'react';
 
 export default function App() {
-  const [length, setLength] = useState(12);
+  const [length, setLength] = useState(32); // Updated password length to 32
   const [charset, setCharset] = useState('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]\\:;?><,./-=');
+  const [selectedCharacters, setSelectedCharacters] = useState([]);
 
   const generatePassword = () => {
     let password = '';
     for (let i = 0; i < length; i++) {
-      password += charset.charAt(Math.floor(Math.random() * charset.length));
+      password += selectedCharacters[Math.floor(Math.random() * selectedCharacters.length)];
     }
     return password;
+  };
+
+  const toggleCharacterSelection = (character) => {
+    if (selectedCharacters.includes(character)) {
+      setSelectedCharacters(selectedCharacters.filter((c) => c !== character));
+    } else {
+      setSelectedCharacters([...selectedCharacters, character]);
+    }
   };
 
   return (
@@ -20,12 +30,14 @@ export default function App() {
       <Picker
         selectedValue={length}
         style={{ height: 50, width: 100 }}
-        onValueChange={(itemValue, itemIndex) => setLength(itemValue)}
+        onValueChange={(itemValue) => setLength(itemValue)}
+        itemStyle={{ color: 'blue' }} // Added color style for picker items
       >
         <Picker.Item label="8" value={8} />
         <Picker.Item label="12" value={12} />
         <Picker.Item label="16" value={16} />
         <Picker.Item label="20" value={20} />
+        <Picker.Item label="32" value={32} /> // Added option for password length 32
       </Picker>
       <Text>Enter the characters to include in the password:</Text>
       <TextInput
@@ -33,6 +45,19 @@ export default function App() {
         value={charset}
         onChangeText={(text) => setCharset(text)}
       />
+      <View style={styles.characterContainer}>
+        {charset.split('').map((character) => (
+          <Button
+            key={character}
+            title={character}
+            onPress={() => toggleCharacterSelection(character)}
+            style={[
+              styles.characterButton,
+              selectedCharacters.includes(character) && styles.selectedCharacterButton,
+            ]}
+          />
+        ))}
+      </View>
       <Button title="Generate Password" onPress={() => alert(generatePassword())} />
       <StatusBar style="auto" />
     </View>
@@ -53,4 +78,18 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
   },
+  characterContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  characterButton: {
+    margin: 5,
+  },
+  selectedCharacterButton: {
+    backgroundColor: 'blue',
+    color: 'white',
+  },
 });
+
