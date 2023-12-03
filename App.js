625ceaf08d8acfda432dfcf6442f3ca90b0500c5
Roxy <114163466+Ry0x7@users.noreply.github.com>
2023-12-03 18:00:30 +0800

    Refactor password generation and picker styling

--- App.js
+++ App.js
@@ -1,27 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar'; // Import StatusBar from expo-status-bar
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
-import React, { useState } from 'react';
+import React, { useState } from 'react'; // Remove duplicate import statement for Animated
 
 export default function App() {
-  const [length, setLength] = useState(32); // Updated password length to 32
+  const [length, setLength] = useState(32);
   const [charset, setCharset] = useState('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]\\:;?><,./-=');
-  const [selectedCharacters, setSelectedCharacters] = useState([]);
+  const [password, setPassword] = useState('');
 
   const generatePassword = () => {
-    let password = '';
+    let newPassword = '';
     for (let i = 0; i < length; i++) {
-      password += selectedCharacters[Math.floor(Math.random() * selectedCharacters.length)];
+      newPassword += charset[Math.floor(Math.random() * charset.length)];
     }
-    return password;
-  };
-
-  const toggleCharacterSelection = (character) => {
-    if (selectedCharacters.includes(character)) {
-      setSelectedCharacters(selectedCharacters.filter((c) => c !== character));
-    } else {
-      setSelectedCharacters([...selectedCharacters, character]);
-    }
+    setPassword(newPassword);
   };
 
   return (
@@ -31,13 +23,13 @@ export default function App() {
         selectedValue={length}
         style={{ height: 50, width: 100 }}
         onValueChange={(itemValue) => setLength(itemValue)}
-        itemStyle={{ color: 'blue' }} // Added color style for picker items
+        itemStyle={{ color: 'blue' }}
       >
         <Picker.Item label="8" value={8} />
         <Picker.Item label="12" value={12} />
         <Picker.Item label="16" value={16} />
         <Picker.Item label="20" value={20} />
-        <Picker.Item label="32" value={32} /> // Added option for password length 32
+        <Picker.Item label="32" value={32} />
       </Picker>
       <Text>Enter the characters to include in the password:</Text>
       <TextInput
@@ -45,29 +37,25 @@ export default function App() {
         value={charset}
         onChangeText={(text) => setCharset(text)}
       />
-      <View style={styles.characterContainer}>
-        {charset.split('').map((character) => (
-          <Button
-            key={character}
-            title={character}
-            onPress={() => toggleCharacterSelection(character)}
-            style={[
-              styles.characterButton,
-              selectedCharacters.includes(character) && styles.selectedCharacterButton,
-            ]}
-          />
-        ))}
+      <Button title="Generate Password" onPress={generatePassword} style={styles.button} />
+      <View style={styles.output}>
+        <TextInput
+          style={styles.outputText}
+          value={password}
+          editable={true}
+        />
       </View>
-      <Button title="Generate Password" onPress={() => alert(generatePassword())} />
       <StatusBar style="auto" />
     </View>
   );
+
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -78,18 +66,20 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
   },
-  characterContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    marginTop: 10,
+  output: {
+    height: 40,
+    width: '80%',
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
-  characterButton: {
-    margin: 5,
+  outputText: {
+    flex: 1,
   },
-  selectedCharacterButton: {
+  button: {
     backgroundColor: 'blue',
     color: 'white',
+    padding: 10,
+    borderRadius: 5,
   },
 });
-
